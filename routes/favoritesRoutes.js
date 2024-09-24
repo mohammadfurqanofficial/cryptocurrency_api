d7b0bc307bc2e4a4145d0ddc414a8a57b3173bcf
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-09-24 15:19:11 +0500

    function name mis spelled

--- routes/favoritesRoutes.js
+++ routes/favoritesRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware"); // Assuming you have an auth middleware
-const { addToFavorites, removeFavorite } = require("../controllers/favoritesController");
+const { addToFavorites, removeFromFavorites } = require("../controllers/favoritesController");
 
 const router = express.Router();
 
 // Protect route ensures the user is authenticated
 router.post("/add", protect, addToFavorites);
+
 // Route to remove a favorite
-router.post('/remove', protect, removeFavorite); // Protect the route with authentication middleware
+router.post('/remove', protect, removeFromFavorites); // Protect the route with authentication middleware
 
 
 module.exports = router;
