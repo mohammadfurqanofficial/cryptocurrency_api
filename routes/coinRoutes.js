9d8b5ee9492dd2252cd851f4a9773711993b6efc
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-09-25 18:08:30 +0500

    remove get coins updates

--- routes/coinRoutes.js
+++ routes/coinRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { getCoinUpdates, saveCoinHistory } = require('../controllers/coinController');
+const { saveCoinHistory } = require('../controllers/coinController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 // GET route to fetch coin updates
-router.get('/updates', protect, getCoinUpdates);
+// router.get('/updates', protect, getCoinUpdates);
 
 // POST route to save coin history to the database
 router.post('/save-history', protect, saveCoinHistory);
