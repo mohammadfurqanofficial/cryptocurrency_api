a79cc03ad8aba125929faef1a4fa65c42212291b
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-09-25 18:05:52 +0500

    added protect to {}

--- routes/coinRoutes.js
+++ routes/coinRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getCoinUpdates, saveCoinHistory } = require('../controllers/coinController');
-const protect = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
