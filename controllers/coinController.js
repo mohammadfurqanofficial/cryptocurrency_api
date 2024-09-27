86f8c6c7b69616b3705ff4aaf7166b8bb9421ea8
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-09-27 20:53:24 +0500

    check coins

--- controllers/coinController.js
+++ controllers/coinController.js
@@ -31,18 +31,14 @@ exports.getCoinHistory = async (req, res) => {
   if (!userId) {
     return res.status(400).json({ message: "Invalid user" });
   }
-  console.log("Querying with coinId:", coinId, "and userId:", userId);
 
   try {
-    // Fetch all favorite coins for the user and coin ID
-    const favorites = await FavoriteCoin.find({ userId, coinId: Number(coinId) });
 
-    if (!favorites.length) {
-      console.log("No favorites found for this coin and user.");
-      return res.status(404).json({ message: "No favorite coin found for this coin" });
-    }
+      // Find all coin history records based on the coin ID
+      const coinHistory = await CoinHistory.find({ coinId });
 
-    console.log("Favorite Coin Details:", favorites);
+      // Fetch all favorite coins for the user
+      const favorites = await FavoriteCoin.findOne({ coinId });
 
       if (!coinHistory.length) {
           return res.status(404).json({ message: "No coin history found for this coin" });
