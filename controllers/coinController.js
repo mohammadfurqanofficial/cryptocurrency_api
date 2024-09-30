012d86c08ac2cdcd5d7263b61662f4efb598d28b
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-09-30 15:27:00 +0500

    get all user favorites

--- controllers/coinController.js
+++ controllers/coinController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const FavoriteCoin = require('../models/FavoriteCoin');
 const CoinHistory = require('../models/CoinHistory');
+const User = require('../models/User');
 
 // Function to get all coin history
 exports.getAllCoinHistory = async (req, res) => {
@@ -46,10 +47,6 @@ exports.getCoinHistory = async (req, res) => {
   }
 };
 
-
-
-
-
 // Function to save coin history from favorite coins
 exports.saveCoinHistory = async (req, res) => {
   const userId = req.user ? req.user.id : null;
@@ -59,61 +56,68 @@ exports.saveCoinHistory = async (req, res) => {
   }
 
   try {
-    // Fetch all favorite coins for the user
-    const favorites = await FavoriteCoin.find({ userId });
+    // Find the user by ID and populate the favoriteCoins array
+    const user = await User.findById(userId).populate({
+      path: 'favoriteCoins', // Populate favoriteCoins field
+    });
+    
+    res.status(200).json({ message: "Get all favorites", user });
 
-    if (!favorites.length) {
-      return res.status(404).json({ message: "No favorite coins found for this user" });
-    }
+    // Fetch all favorite coins for the user
+    // const favorites = await FavoriteCoin.find({ userId });
+
+    // if (!favorites.length) {
+    //   return res.status(404).json({ message: "No favorite coins found for this user" });
+    // }
 
     // Extract coin IDs from favorites
-    const coinIds = favorites.map(fav => fav.coinId).join(',');
+    // const coinIds = favorites.map(fav => fav.coinId).join(',');
 
     // Fetch updates from CoinMarketCap API
-    const response = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=${coinIds}`, {
-      headers: {
-        'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY, // Ensure API key is correctly set
-      },
-    });
+    // const response = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=${coinIds}`, {
+    //   headers: {
+    //     'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY, // Ensure API key is correctly set
+    //   },
+    // });
 
-    const coins = response.data.data;
+    // const coins = response.data.data;
 
-    if (!coins || Object.keys(coins).length === 0) {
-      return res.status(404).json({ message: "No coin data found" });
-    }
+    // if (!coins || Object.keys(coins).length === 0) {
+    //   return res.status(404).json({ message: "No coin data found" });
+    // }
 
     // Iterate over the coins and save only the quote data in CoinHistory
-    for (const id of Object.keys(coins)) {
-      const coin = coins[id];
-      const coinQuote = coin.quote.USD;
+    // for (const id of Object.keys(coins)) {
+    //   const coin = coins[id];
+    //   const coinQuote = coin.quote.USD;
 
-      // Create and save a new CoinHistory entry with only the quote details
-      const coinHistory = new CoinHistory({
-        coinId: coin.id,
-        price: coinQuote.price,
-        volume_24h: coinQuote.volume_24h,
-        percent_change_1h: coinQuote.percent_change_1h,
-        percent_change_24h: coinQuote.percent_change_24h,
-        percent_change_7d: coinQuote.percent_change_7d,
-        percent_change_30d: coinQuote.percent_change_30d,
-        percent_change_60d: coinQuote.percent_change_60d,
-        percent_change_90d: coinQuote.percent_change_90d,
-        market_cap: coinQuote.market_cap,
-        fully_diluted_market_cap: coinQuote.fully_diluted_market_cap,
-        lastUpdated: new Date(coin.last_updated),
-      });
+    //   // Create and save a new CoinHistory entry with only the quote details
+    //   const coinHistory = new CoinHistory({
+    //     coinId: coin.id,
+    //     price: coinQuote.price,
+    //     volume_24h: coinQuote.volume_24h,
+    //     percent_change_1h: coinQuote.percent_change_1h,
+    //     percent_change_24h: coinQuote.percent_change_24h,
+    //     percent_change_7d: coinQuote.percent_change_7d,
+    //     percent_change_30d: coinQuote.percent_change_30d,
+    //     percent_change_60d: coinQuote.percent_change_60d,
+    //     percent_change_90d: coinQuote.percent_change_90d,
+    //     market_cap: coinQuote.market_cap,
+    //     fully_diluted_market_cap: coinQuote.fully_diluted_market_cap,
+    //     lastUpdated: new Date(coin.last_updated),
+    //   });
 
-      // Save the coinHistory to the database
-      const savedCoinHistory = await coinHistory.save(); 
+    //   // Save the coinHistory to the database
+    //   const savedCoinHistory = await coinHistory.save(); 
 
-      // Update the corresponding FavoriteCoin with the coinHistoryId
-      await FavoriteCoin.findOneAndUpdate(
-        { userId, coinId: coin.id }, // Match the favorite coin
-        { coinHistoryId: savedCoinHistory._id } // Update with the coinHistory ID
-      );
-    }
+    //   // Update the corresponding FavoriteCoin with the coinHistoryId
+    //   await FavoriteCoin.findOneAndUpdate(
+    //     { userId, coinId: coin.id }, // Match the favorite coin
+    //     { coinHistoryId: savedCoinHistory._id } // Update with the coinHistory ID
+    //   );
+    // }
 
-    res.status(200).json({ message: "Coin quotes saved successfully" });
+    // res.status(200).json({ message: "Coin quotes saved successfully", user });
   } catch (error) {
     console.error("Error saving coin quotes:", error.response ? error.response.data : error.message);
     res.status(500).json({ message: "Server error", error: error.message });
