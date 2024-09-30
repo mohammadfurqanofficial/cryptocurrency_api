ddf747de1998e358308501c5ee6f87e4246925b3
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-09-30 20:14:59 +0500

    added 60 seconds code into index to get update coin update

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+setInterval(() => {
+  saveCoinHistory();
+  checkAndSendAlerts();
+
+  console.log("chamou");
+}, 60000);
+
 // Listen to port from .env or default to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
