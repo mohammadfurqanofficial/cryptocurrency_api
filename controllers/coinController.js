c1437c264d1cbc4b45b10029010a08b90a57cd59
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-09-30 16:38:39 +0500

    get coin history ID

--- controllers/coinController.js
+++ controllers/coinController.js
@@ -66,7 +66,7 @@ exports.saveCoinHistory = async (req, res) => {
       return res.status(404).json({ message: "No favorite coins found for this user" });
     }
     const favorites = user.favoriteCoins;
-
+    
     if (!favorites.length) {
       return res.status(404).json({ message: "No favorite coins found for this user" });
     }
@@ -118,7 +118,7 @@ exports.saveCoinHistory = async (req, res) => {
       );
     }
 
-    res.status(200).json({ message: "Coin quotes saved successfully", coinHistory: savedCoinHistory });
+    res.status(200).json({ message: "Coin quotes saved successfully", coinHistoryId });
   } catch (error) {
     console.error("Error saving coin quotes:", error.response ? error.response.data : error.message);
     res.status(500).json({ message: "Server error", error: error.message });
