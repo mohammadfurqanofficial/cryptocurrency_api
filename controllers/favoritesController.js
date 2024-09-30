f1b8138df641ae397d4914774b648a06fb7281ad
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-09-30 14:59:23 +0500

    added user Id to get coin using user ID

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -8,7 +8,7 @@ exports.getallFavorites = async (req, res) => {
 
   try {
       // Find all favorite coins for the user and populate the CoinHistory
-      const favorites = await FavoriteCoin.find()
+      const favorites = await FavoriteCoin.find({ userId })
           .populate('coinHistoryId'); // Ensure 'coinHistoryId' is correctly set in schema
 
       if (!favorites.length) { // Check if there are no favorites
