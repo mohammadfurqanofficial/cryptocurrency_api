778bf17857446d56f6ef6095e5bbea25dc31f1f6
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-10-05 21:02:01 +0500

    code updated in cron.js

--- routes/cron.js
+++ routes/cron.js
@@ -1,4 +1,10 @@
-export default function handler() {
-    console.log("This is cron API");
-    response.status(200).json({ success: true });
-}
+const express = require('express');
+const router = express.Router();
+
+// Define your cron route
+router.get('/save-coin-history', (req, res) => {
+  console.log("This is cron API");
+  res.status(200).json({ success: true });
+});
+
+module.exports = router;
