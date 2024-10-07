7b62eb6e197b74a5cc89605d3069b9262c113606
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-10-07 21:06:01 +0500

    removed cron.js

--- routes/cron.js
+++ routes/cron.js
@@ -11,17 +11,17 @@
 //     console.log("This is cron after API");
 //     res.status(200).end('Hello Cron!');
 // }
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-// Define your cron route
-router.get('/save-coin-history', (req, res) => {
-  console.log("This is cron API");
-  saveCoinHistory();
-  res.status(200).json({ success: true });
-});
+// // Define your cron route
+// router.get('/save-coin-history', (req, res) => {
+//   console.log("This is cron API");
+//   saveCoinHistory();
+//   res.status(200).json({ success: true });
+// });
 
-module.exports = router;
+// module.exports = router;
 
 // module.exports = (req, res) => {
 //     console.log("This is cron API");
