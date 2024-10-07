fbbb73b03d289d96b332e9be46e3655aa871f50f
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-10-07 22:27:55 +0500

    Removed comments and extra code from index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const authRoutes = require('./routes/authRoutes');
 const cryptoRoutes = require("./routes/cryptoRoutes"); // Import the new crypto routes
 const favoritesRoutes = require('./routes/favoritesRoutes'); // New favorite route
 const coinRoutes = require('./routes/coinRoutes'); // New favorite route
-import { cron } from './routes/cron.js';
 const cors = require('cors');
 
 // Load environment variables
@@ -21,10 +20,10 @@ app.use(express.json()); // For parsing application/json
 app.use(cors()); // Enable CORS
 
 // Routes
-app.use('/api/auth', authRoutes); // Authentication routes
-app.use("/api", cryptoRoutes); // Add the cryptocurrency routes here
-app.use("/api/favorites", favoritesRoutes); // Add this line
-app.use("/api/coins", coinRoutes); // Add this line
+app.use('/api/auth', authRoutes);
+app.use("/api", cryptoRoutes);
+app.use("/api/favorites", favoritesRoutes);
+app.use("/api/coins", coinRoutes);
 
 
 // Default Route
