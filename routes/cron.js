ebc2d17f5c28ca2e8d1475470f46057c6f9aea91
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-10-07 20:43:14 +0500

    updated cron in vercel.json

--- routes/cron.js
+++ routes/cron.js
@@ -1,22 +1,27 @@
-export const cron = () => {
-    console.log("This is cron API");
-    saveCoinHistory();
-    console.log("This is cron after API");
-    res.status(200).json({ success: true });
-}
-
-
+// export const cron = () => {
+//     console.log("This is cron API");
+//     saveCoinHistory();
+//     console.log("This is cron after API");
+//     res.status(200).json({ success: true });
+// }
 // const express = require('express');
-// const router = express.Router();
+// export default function handler(req, res) {
+//     console.log("This is cron API");
+//     saveCoinHistory();
+//     console.log("This is cron after API");
+//     res.status(200).end('Hello Cron!');
+// }
+const express = require('express');
+const router = express.Router();
 
 // Define your cron route
-// router.get('/save-coin-history', (req, res) => {
-//   console.log("This is cron API");
-//   saveCoinHistory();
-//   res.status(200).json({ success: true });
-// });
+router.get('/save-coin-history', (req, res) => {
+  console.log("This is cron API");
+  saveCoinHistory();
+  res.status(200).json({ success: true });
+});
 
-// module.exports = router;
+module.exports = router;
 
 // module.exports = (req, res) => {
 //     console.log("This is cron API");
