a13009ffd6fad5e7ab9fec366cfb4853e0c8fe7d
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-10-08 16:53:44 +0500

    Added new route to get save-history api into it to run cron job

--- routes/coinRoutes.js
+++ routes/coinRoutes.js
@@ -16,4 +16,19 @@ router.get('/coin-history/download/:coinId', protect, getCoinHistoryDownload);
 // Route to get coin history by coin ID
 router.get('/all-coins-history', protect, getAllCoinHistory);
 
+// Cron API
+// Create a GET route specifically for the cron job
+router.get('/save-history-cron', async (req, res) => {
+    try {
+      // Internally call the POST route to trigger the saveCoinHistory function
+      const response = await axios.post(`${process.env.API_URL}/api/coins/save-history`);
+      console.log(response);
+      res.status(200).json({ message: "Cron job triggered successfully", data: response.data });
+    } catch (error) {
+      console.error("Error triggering cron job:", error.message);
+      res.status(500).json({ message: "Error triggering cron job", error: error.message });
+    }
+  });
+
+
 module.exports = router;
